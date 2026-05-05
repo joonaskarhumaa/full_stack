9f3691f6b4c272bd057f1ddbe3434d212d524406
Joonas Karhumaa <joonas.karhumaa@tuni.fi>
2026-05-05 12:05:48 +0300

    make unicafe step1

---
+++ osa_1/unicafe/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const Header = ({ header }) => {
+  return (
+    <h1>{header}</h1>
+  )
+}
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <Header header="give feedback" />
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />      
+      <Header header="statistics" />
+      <Statistics
+      good={good} neutral={neutral} bad={bad}
+      />
+
+    </div>
+  )
+}
+
+export default App
