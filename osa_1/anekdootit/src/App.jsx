0b66bbbc64f64d0617030f2c07921af30abba95a
Joonas Karhumaa <joonas.karhumaa@tuni.fi>
2026-05-05 12:45:38 +0300

    make anekdootit step2

--- osa_1/anekdootit/src/App.jsx
+++ osa_1/anekdootit/src/App.jsx
@@ -13,15 +13,24 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const randomAnecdote = () => {
     const random = Math.floor(Math.random() * anecdotes.length)
     setSelected(random)
   }
 
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={vote}>vote</button>
       <button onClick={randomAnecdote}>next anecdote</button>
     </div>
   )
